d96542fabf2c23c90af3689e6761b3a841b1119d
iMocco <taoxin167@gmail.com>
2018-10-27 13:12:35 +0800

    添加antd高级配置

---
+++ blog-react/config-overrides.js
@@ -0,0 +1,11 @@
+/*
+* @Author: iMocco
+* @Date:   2018-10-27 13:08:20
+* @Last Modified by:   iMocco
+* @Last Modified time: 2018-10-27 13:08:23
+*/
+// 修改默认配置
+module.exports = function override(config, env) {
+  // do stuff with the webpack config...
+  return config;
+};
--- blog-react/src/App.js
+++ blog-react/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { DatePicker } from 'antd';
+
+import { Button } from 'antd';
+
 import 'antd/dist/antd.css'
 import logo from './logo.svg';
 import './App.css';
@@ -8,21 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <DatePicker />
-        </header>
+          <div>
+            <Button type="primary">Primary</Button>
+            <Button>Default</Button>
+            <Button type="dashed">Dashed</Button>
+            <Button type="danger">Danger</Button>
+          </div>
       </div>
     );
   }
