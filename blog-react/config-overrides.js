b2d4dcfc0c843757da83a8ac04ec1138cb5f2a40
iMocco <taoxin167@gmail.com>
2018-10-27 13:16:36 +0800

    使用 babel-plugin-import 已按需加载

--- blog-react/config-overrides.js
+++ blog-react/config-overrides.js
@@ -1,11 +1,15 @@
 /*
-* @Author: iMocco
-* @Date:   2018-10-27 13:08:20
-* @Last Modified by:   iMocco
-* @Last Modified time: 2018-10-27 13:08:23
-*/
+ * @Author: iMocco
+ * @Date:   2018-10-27 13:08:20
+ * @Last Modified by:   iMocco
+ * @Last Modified time: 2018-10-27 13:14:46
+ */
 // 修改默认配置
+const { injectBabelPlugin } = require('react-app-rewired');
+
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
-  return config;
+	config = injectBabelPlugin(
+		['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+		config, );
+	return config;
 };
--- blog-react/src/App.js
+++ blog-react/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 import { Button } from 'antd';
 
-import 'antd/dist/antd.css'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
