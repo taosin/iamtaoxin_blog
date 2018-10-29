6d5e850ee65a40c6920465c27515050f0826d496
iMocco <taoxin167@gmail.com>
2018-10-29 16:19:29 +0800

    m

--- blog-react/src/App.js
+++ blog-react/src/App.js
@@ -4,16 +4,12 @@ import { Button } from "antd";
 import "./App.css";
 import Home from './Pages/Home/home'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Home></Home>
-        <Switch>
-        <Route path="/home" exact component={Home} />
-        </Switch>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className = "App" >
+				<Home></Home>
+			</div>
+		);
+	}
 }
-
 export default App;
